a4b5236623c6857c7be657c4d0d15ed8ac2b7b1c
Nick Albano <nick@Nicks-MacBook-Pro.local>
2026-04-10 13:40:45 -0400

    Final fixes

---
+++ api/Migrations/20260410174001_AddTunnelMailboxListIndex.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AFHSync.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTunnelMailboxListIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_contact_sync_state_tunnel_id",
+                table: "contact_sync_state");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_contact_sync_state_tunnel_mailbox_list",
+                table: "contact_sync_state",
+                columns: new[] { "tunnel_id", "target_mailbox_id", "phone_list_id" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "idx_contact_sync_state_tunnel_mailbox_list",
+                table: "contact_sync_state");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_contact_sync_state_tunnel_id",
+                table: "contact_sync_state",
+                column: "tunnel_id");
+        }
+    }
+}
